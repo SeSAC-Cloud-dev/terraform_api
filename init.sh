d3ecdb709bba685f1e25098177d2fff1e9f610c6
highfreshness <meteo0718@gmail.com>
2024-08-23 13:01:38 +0900

    Fix : init.sh

--- init.sh
+++ init.sh
@@ -13,3 +13,5 @@ wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/ke
 
 
 python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt 
+
+uvicorn main:app --host 0.0.0.0
