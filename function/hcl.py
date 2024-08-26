f9323566d4b0e6f5c412bccfed9afb99bd96206c
highfreshness <meteo0718@gmail.com>
2024-08-26 20:29:08 +0900

    🔨Fix : API ouput 수정

--- function/hcl.py
+++ function/hcl.py
@@ -1,9 +1,13 @@
 import re
 import os
+import json
 import asyncio
 from typing import List
+from ast import literal_eval
 
-
+# Secret
+KEY_PATH = "./key.pem"
+    
 def remove_ansi_escape_sequences(text: str) -> str:
     # ANSI escape sequences 패턴
     ansi_escape_pattern = re.compile(r"\x1B[@-_][0-?]*[ -/]*[@-~]")
@@ -39,10 +43,15 @@ def create_hcl(user_config: dict) -> str:
         tags = {{
             Name = "{user_config['user_id']+'_'+user_config['seq']}"
         }}
+        get_password_data = true
+    }}
+
+    output "instance_id" {{
+      value = aws_instance.EC2.id
     }}
 
     output "instance_private_ip" {{
-        value = jsonencode({{private_ip = aws_instance.EC2.private_ip}})
+        value = aws_instance.EC2.private_ip
     }}
     """
 
@@ -57,6 +66,10 @@ def create_hcl(user_config: dict) -> str:
         file.write(terraform_config)
     return output_path
 
+async def decrypt_password(instance_id, key_path):
+    decrypt_command = ["aws", "ec2", "get-password-data", "--instance-id", f"{instance_id}", "--priv-launch-key", f"{key_path}"]
+    decrypt_process = await run_command(decrypt_command)
+    return decrypt_process
 
 async def run_command(command: List[str]):
     process = await asyncio.create_subprocess_exec(
@@ -76,19 +89,25 @@ async def terraform_apply(output_path: str) -> str:
     apply_process = await run_command(apply_command)
 
     result = remove_ansi_escape_sequences(apply_process)
-
-    match = re.search(r'private_ip\\":\\"(.*?)\\"', result)
-
-    if match:
-        private_ip = match.group(1)
-    else:
-        print("Public IP 정보를 찾을 수 없습니다.")
-
-    return {"message": result, "private_ip": private_ip}
+    
+    # Terraform 결과 추출
+    terraform_result_path = os.path.join(output_path, "terraform.tfstate")
+    with open(terraform_result_path, 'r') as file:
+        result_data = json.load(file)
+        
+    instance_id = result_data['outputs']['instance_id']['value']
+    instance_private_ip = result_data['outputs']['instance_private_ip']['value']
+    
+    # Password 복호화
+    pass_data = await decrypt_password(instance_id, KEY_PATH)
+    password = literal_eval(pass_data)['PasswordData']
+    
+    # Guacamole VD 연결 API
+    return {"result" : result, "instance_id" : instance_id, "private_ip" : instance_private_ip, "password_data" : password}
 
 
 async def terraform_destroy(work_dir: str) -> str:
     destroy_command = ["terraform", f"-chdir={work_dir}", "destroy", "--auto-approve"]
     destroy_process = await run_command(destroy_command)
     result = remove_ansi_escape_sequences(destroy_process)
-    return result
+    return {"message" : result}
