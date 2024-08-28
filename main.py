e4e49a6dbb4ed060b5d04a7147c8237ef22e74b4
highfreshness <meteo0718@gmail.com>
2024-08-28 14:46:13 +0900

    Fix : Log 시간 확인 기능 추가

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from fastapi import FastAPI
+import logging
+import datetime
+from fastapi import FastAPI, Request
 from router import hcl_maker
+
+# 로그 설정
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 app.include_router(hcl_maker.router)
 app.router.redirect_slashes = False
 
+# 미들웨어로 작업 완료 시점 기록
+@app.middleware("http")
+async def log_completion_time(request: Request, call_next):
+    response = await call_next(request)  # 요청을 처리하고 응답 생성
+    completion_time = datetime.datetime.now().isoformat()  # 작업 완료 시점 기록
+    logger.info(f"Request: {request.method} {request.url} - Completed at {completion_time}")
+    return response
 
 @app.get("/")
 async def root():
